f015056b98e19707716ca49ce4dcde35cef02a77
Fernando Herman Pookey <nandopookey@gmail.com>
2023-05-09 20:46:29 +0700

    Email Verify Almost Done

---
+++ resources/views/user/layouts/app-email-verify.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="keywords" content="HTML5 Template">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>@yield('title')</title>
+
+    {{-- Style --}}
+    @stack('prepend-style')
+    @include('user.includes.style')
+    @stack('addon-style')
+
+</head>
+
+<body>
+
+    {{-- Page Content --}}
+    @yield('content')
+
+    {{-- Script --}}
+    @stack('prepend-script')
+    @include('user.includes.script')
+    @stack('addon-script')
+
+</body>
+
+</html>
